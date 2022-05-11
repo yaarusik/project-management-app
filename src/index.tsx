e93b8141d5aca31e2cf857ab57667cab47a475e5
Elena Shustova <alenyf-ka@mail.ru>
2022-05-11 10:30:50 +0300

    fix: move files in folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './store';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
